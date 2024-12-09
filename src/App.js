c559ce604ede71f84e6a1b1aa6f231742afa84eb
YuuuDays <144862458+YuuHikida@users.noreply.github.com>
2024-12-09 11:08:42 +0000

    componetization

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// componentNameは最初は大文字にする
+export const App = () => {
+  return (
+    // <React.Fragment>
+    <>
+      <h1>こん^^</h1>
+      <p>お元気ですかぁ？</p>
+    </>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-
-// componentNameは最初は大文字にする
-const App = () => {
-  return <h1>こん^^</h1>;
-};
 
 root.render(
   <StrictMode>
