274c8d25834692a8f725cf3b51e1501313424d9b
YuuuDays <144862458+YuuHikida@users.noreply.github.com>
2024-12-09 11:02:47 +0000

    ref

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import { createRoot } from "react-dom/client";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
+// componentNameは最初は大文字にする
+const App = () => {
+  return <h1>こん^^</h1>;
+};
+
 root.render(
   <StrictMode>
-    <h1>こんにちは</h1>
+    <App />
   </StrictMode>
 );
