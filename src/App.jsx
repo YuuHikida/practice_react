51517d29f7d19ce8589975ecbb98297f1e8a58ab
YuuuDays <144862458+YuuHikida@users.noreply.github.com>
2024-12-11 12:10:37 +0000

    inprogress useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 // componentNameは最初は大文字にする(大文字のパスカルケースにしないと読み込まない)
 export const App = () => {
+  console.log("--- APP ---");
   const [num, setNum] = useState(0);
+  const [isShowFace, setIsShowFace] = useState(false);
   const onClickCountUp = () => {
-    setNum((prev) => prev + 1);
     setNum((prev) => prev + 1);
     //setNum(num + 1);  //レンダリング後に上から関数を再度読み込むのでuseState(0)が評価され左の(num+1)を複数用意しても増減しない
   };
+
+  const onClickToggle = () => {
+    setIsShowFace(!isShowFace);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        isShowFace || setIsShowFace(true);
+      } else {
+        isShowFace && setIsShowFace(false);
+      }
+    }
+  }, [num]); //useStateの第二引数は配列、これを依存配列という
 
   return (
     // <React.Fragment>
@@ -19,6 +34,8 @@ export const App = () => {
       {/* <ColorfulMessage color="green" message="はい、元気です" /> */}
       <button onClick={onClickCountUp}>カウントアップ</button>
       <p>{num}</p>
+      <button onClick={onClickToggle}>on/off</button>
+      {isShowFace && <p>(≧▽≦)</p>}
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -2,7 +2,7 @@ export const ColorfulMessage = (props) => {
   //console.log(props); ->{color: 'blue', message: 'お元気ですかぁ？'}
   //オブジェクトの分割代入
   const { color, children } = props;
-
+  console.log("--- ColorFUllMessage ---"); //親再レンダリングされるとこちらもレンダリングされる
   const contentStyleA = {
     color: color,
     fontSize: "20px",
