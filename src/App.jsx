f047ce5ee698aec5b4d5c47b5bcad9024a8a9fc2
YuuuDays <144862458+YuuHikida@users.noreply.github.com>
2024-12-11 11:44:07 +0000

    inprogress useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 // componentNameは最初は大文字にする(大文字のパスカルケースにしないと読み込まない)
 export const App = () => {
-  const onClickButton = () => alert();
+  const [num, setNum] = useState(0);
+  const onClickCountUp = () => {
+    setNum(111);
+  };
 
   const contentStyleB = {
     color: "blue",
@@ -16,7 +20,8 @@ export const App = () => {
       <ColorfulMessage color="blue">お元気ですかぁ？</ColorfulMessage>
       <ColorfulMessage color="green">はい、元気です</ColorfulMessage>
       {/* <ColorfulMessage color="green" message="はい、元気です" /> */}
-      <button onClick={onClickButton}>button</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,6 +1,6 @@
 export const ColorfulMessage = (props) => {
   //console.log(props); ->{color: 'blue', message: 'お元気ですかぁ？'}
-  //オブジェクトの分割だいにゅウ
+  //オブジェクトの分割代入
   const { color, children } = props;
 
   const contentStyleA = {
